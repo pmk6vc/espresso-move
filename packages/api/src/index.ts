1cd983f61ac251705b5a00c7a5c273b6474264a3
Varun Kulkarni <varunkulkarni@gmail.com>
2023-11-17 17:59:43 -0500

    get imports across packages working

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import helloWorld from "@espresso-move/utils"
 
+helloWorld()
 const app = express();
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to your service!");
