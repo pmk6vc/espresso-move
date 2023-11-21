8634bfda488affce446e11dad95723402746f4a8
Varun Kulkarni <varunkulkarni@gmail.com>
2023-11-21 12:51:57 -0500

    use new pool with unique name

--- packages/api/infra/modules/iam/main.tf
+++ packages/api/infra/modules/iam/main.tf
@@ -9,28 +9,6 @@ resource "google_service_account" "service_account" {
 }
 
 /**
-* Create OpenID provider to manage keyless authentication for GitHub CI/CD workflows
-* Needs to be applied before service account can authenticate in GitHub Actions
-* See resources for more details:
-* - GCP page: https://cloud.google.com/blog/products/identity-security/enabling-keyless-authentication-from-github-actions
-* - Example: https://github.com/terraform-google-modules/terraform-google-github-actions-runners/tree/master/modules/gh-oidc
-*
-* NOTE - cannot move this to separate module, since sensitive SA outputs cannot be used in terraform for_each arguments
-*/
-module "oidc" {
-  source = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
-  project_id = var.gcp_project_id
-  pool_id = "tf-managed-pool"
-  provider_id = "tf-managed-pool-provider"
-  sa_mapping = {
-    (google_service_account.service_account.account_id) = {
-      sa_name = google_service_account.service_account.name
-      attribute = "attribute.repository/pmk6vc"
-    }
-  }
-}
-
-/**
 * Assign the role required to manage IAM policies
 * Needs to be applied before this service account can be used to manage other IAM policies during CI/CD
 */
@@ -62,22 +40,12 @@ resource "google_project_iam_member" "workload_identity_user" {
 }
 
 /**
-* Assign the role required to fetch service account IAM policies for applying Terraform changes
+* Assign the role required to manage service account IAM policies for applying Terraform changes
 * Needs to be applied before this service account can be used to manage cloud resources during CI/CD
 */
-resource "google_project_iam_member" "service_account_viewer" {
+resource "google_project_iam_member" "service_account_admin" {
   project = var.gcp_project_id
-  role    = "roles/iam.serviceAccountViewer"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
-}
-
-/**
-* Assign the role required to fetch workload identity pools
-* Needs to be applied before this service account can be used to manage cloud resources during CI/CD
-*/
-resource "google_project_iam_member" "workload_identity_pools_viewer" {
-  project = var.gcp_project_id
-  role    = "roles/iam.workloadIdentityPoolViewer"
+  role    = "roles/iam.serviceAccountAdmin"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
@@ -144,4 +112,40 @@ resource "google_project_iam_member" "identity_platform_admin" {
   project = var.gcp_project_id
   role = "roles/identityplatform.admin"
   member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+/********
+CICD
+********/
+
+/**
+* Assign the role required to manage workload identity pool resources
+* Needs to be applied before this service account can be used to manage workload identity pools for GitHub Actions auth
+*/
+resource "google_project_iam_member" "workload_identity_pools_admin" {
+  project = var.gcp_project_id
+  role    = "roles/iam.workloadIdentityPoolAdmin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+/**
+* Create OpenID provider to manage keyless authentication for GitHub CI/CD workflows
+* Needs to be applied before service account can authenticate in GitHub Actions
+* See resources for more details:
+* - GCP page: https://cloud.google.com/blog/products/identity-security/enabling-keyless-authentication-from-github-actions
+* - Example: https://github.com/terraform-google-modules/terraform-google-github-actions-runners/tree/master/modules/gh-oidc
+*
+* NOTE - cannot move this to separate module, since sensitive SA outputs cannot be used in terraform for_each arguments
+*/
+module "oidc" {
+  source = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id = var.gcp_project_id
+  pool_id = "managed-pool"
+  provider_id = "managed-pool-gh-provider"
+  sa_mapping = {
+    (google_service_account.service_account.account_id) = {
+      sa_name = google_service_account.service_account.name
+      attribute = "attribute.repository/pmk6vc/espresso-move"
+    }
+  }
 }
